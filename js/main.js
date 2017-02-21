74d7424bd5cb435b39ce0101bcfe4413b0aa9dc4
komnotmr <komnotmr@gmail.com>
2017-02-21 16:23:50 +0700

    Игрок это несколько спрайтов.

--- js/main.js
+++ js/main.js
@@ -40,6 +40,8 @@ function preload()
 	// loading font from google fonts too long loading !!!
 	game.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
 	//load player texture
+	game.load.spritesheet("playerHeadSpriteSheet", "assets/headSpriteSheet.png", 26, 48, 4, 0, 0);
+
 	game.load.image("playerTexture", "assets/playerTexture.png", 50, 100);
 }
 
@@ -57,25 +59,18 @@ function create()
 	text = game.add.text(game.world.centerX, game.world.centerY, "this game", styles[1]);
 		
 	// Player init
-	player.push( create_Player(game, {x:0, y:10}, "#fac", "playerTexture", 300, 0.1));
-/*	
-	player.push( game.add.sprite(0, 10, "playerTexture") );
-	game.physics.arcade.enable(player[0]);
-	player[0].body.bounce.y = 0.1;
-	player[0].body.gravity.y = 300;
-	player[0].body.collideWorldBounds = true;
-	player[0].body.onGround = {onGround: false};
-	player[0].body.maxVelocity = { x: 99999, y: 99999}; 
-*/
+	player.push( createPlayer(game, {x:0, y:10}, "#fac", ["playerHeadSpriteSheet",
+														  "playerBodySpriteSheet",
+														  "playerHandLeftSpriteSheet",
+														  "playerHandRightSpriteSheet",
+														  "playerLegLeftSpriteSheet",
+														  "playerLegRightSpriteSheet"], 300, 0.1));
+	player[0].head.animation.enableUpdate = true;
+	player[0].head.animation.play("stay", 30, true);
 
-	//platforms init
-	/*platforms = game.add.group();
-	platforms.enableBody = true;
-	platforms.collideWoldBounds = true;
-	platforms.physicsBodyType = Phaser.Physics.ARCADE;
-	var p = platforms.create(0, game.world.height - 50, "playerTexture");
-	p.scale.setTo(12, 1);
-	p.body.immovable = true;*/
+	//player[0].body.animation.enableUpdate = true;
+	//player[0].body.animation.play("stay", 4, true);
+
 }
 
 function update()
@@ -85,14 +80,15 @@ function update()
 		case 0:  break;
 		case 1:
 			if (cursors.up.isDown) {
-				if(player[0].body.onGround)
-				player[0].body.position.y -= 6;//game.camera.y += 4;
+				//if(player[0].body.onGround)
+				//player[0].body.position.y -= 6;//game.camera.y += 4;
+				//player[0].animations.play("hand_top", 4, true);
 			} else if (cursors.down.isDown) {
-				player[0].body.position.y += 6;//game.camera.y -= 4;
+				//player[0].body.position.y += 6;//game.camera.y -= 4;
 			} if (cursors.left.isDown) {
-				player[0].body.position.x -= 6;//game.camera.x += 4;
+				//player[0].body.position.x -= 6;//game.camera.x += 4;
 			} else if (cursors.right.isDown) {
-				player[0].body.position.x += 6;//game.camera.x -= 4;
+				//player[0].body.position.x += 6;//game.camera.x -= 4;
 			}
 		break;
 		case 2: break;	
@@ -103,6 +99,6 @@ function render()
 {
 	// TODO check what should we do here
 	//game.debug.geom(player[0].rect, player[0].color);
-	player[0].body.onGround.onGround = game.physics.arcade.collide(player[0], platforms);
-	console.log(player[0].body.onGround.onGround);
+	//player[0].body.onGround.onGround = game.physics.arcade.collide(player[0], platforms);
+	//console.log(player[0].body.onGround.onGround);
 }
--- js/player.js
+++ js/player.js
@@ -1,19 +1,64 @@
 /* player.js */
 
-//  Create player Phaser.game.sprite
-function create_Player(game, position, color, texture_name, gravity, bounce)
+//  Create player Phaser.game.sprite  texture_names[6] - head, body, lArm, rArm, lLeg, rLeg
+function createPlayer(game, position, color, texture_names, gravity, bounce)
 {
-    var p = game.add.sprite(position.x,
+    var main_player,	// Object all sprites
+		
+		p_head_sprite = game.add.sprite(position.x,
                             position.y,
-                            texture_name
-                           );
+                            texture_names[0]
+                           ),
+		/*p_body_sprite = game.add.sprite(position.x,
+                            position.y,
+                            texture_names[1]
+                           ),
+		p_lArm_sprite =  game.add.sprite(position.x,
+                            position.y,
+                            texture_names[2]
+                           ),
+		p_rArm_sprite =  game.add.sprite(position.x,
+                            position.y,
+                            texture_names[3]
+                           ),
+        p_lLeg_sprite =  game.add.sprite(position.x,
+                            position.y,
+                            texture_names[4]
+                           ),
+	    p_rLeg_sprite =  game.add.sprite(position.x,
+                            position.y,
+                            texture_names[5]
+                           ),*/
+		
+	    p_head_sprite_anim = p_head_sprite.animations.add("stay", [0,1,2,3,2,1], 7, true),
+		//p_body_sprite_anim = p_body_sprite.animations.add("stay",  [0,1,2,3,2,1], 5, true);
 
+		main_player = 
+		{
+			head:
+			{
+				animation: p_head_sprite_anim,
+				sprite : p_head_sprite,
+			},
+
+			/*body:
+			{
+				animation: p_body_sprite_anim,
+				sprite : p_body_sprite
+			}*/
+		};
+	
+	init_physics( main_player.head.sprite , bounce, gravity);
+	//init_physics( main_player.body.sprite , bounce, gravity);
+	
+    return main_player;
+}
+
+function init_physics( p , bounce, gravity)
+{
 	game.physics.arcade.enable(p);
 	p.body.bounce.y = bounce;
 	p.body.gravity.y = gravity;
 	p.body.collideWorldBounds = true;
 	p.body.onGround = {onGround: false};
-	p.body.maxVelocity = { x: 99999, y: 99999}; 
-
-    return p;
 }
