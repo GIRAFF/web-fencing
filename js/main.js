06ccf5f391e6179e0201d58fdcada67211d31a3d
GIRAFF <k.yngvarr@gmail.com>
2017-03-03 11:51:55 +0700

    PAUSE on esc.

--- js/main.js
+++ js/main.js
@@ -12,14 +12,19 @@ var game,
 	text,
 	createText, 
 	styles = //styles for different text
-	 [
-		 { font: "72px UnifrakturMaguntia", fill: "#fac" },
-		 { font: "100px UnifrakturMaguntia", fill: "#ff3" }
-	 ],
-	 cursors, // arrows keys
-	 player = [], //players  Phaser.game.sprite
-	 platforms, //platforms  Phaser.game.sprite
-	 gravity = 300; //global player gravity
+	[
+		{ font: "20px UnifrakturMaguntia", fill: "#fac" },
+		{ font: "100px UnifrakturMaguntia", fill: "#ff3" }
+	],
+	//use it for all the keyboard input
+	input =
+	{
+		cursors: null,
+		esc: null
+	}, 
+	player = [], //players  Phaser.game.sprite
+	platforms, //platforms  Phaser.game.sprite
+	gravity = 300; //global player gravity
 
 
 // script for loading fonts from google fonts.
@@ -51,12 +56,17 @@ function init()
 function preload()
 {
 	// loading font from google fonts too long loading !!!
-	game.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
+	game.load.script('webfont',
+		'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
 	//load player texture
-	game.load.spritesheet("playerHeadSpriteSheet", "assets/headSpriteSheet.png", 26, 48, 4, 0, 0);//head
-	game.load.spritesheet("playerBodySpriteSheet", "assets/bodySpriteSheet0.png", 27, 116, 4, 0, 0);//body
-	game.load.spritesheet("playerLegLeftSpriteSheet", "assets/leftLegSpriteSheet.png", 19, 43, 2, 0, 0);//legs right
-	game.load.spritesheet("playerLegRightSpriteSheet", "assets/rightLegSpriteSheet.png", 14, 45, 2, 0, 0);//legs right
+	game.load.spritesheet("playerHeadSpriteSheet",
+		"assets/headSpriteSheet.png", 26, 48, 4, 0, 0);//head
+	game.load.spritesheet("playerBodySpriteSheet",
+		"assets/bodySpriteSheet0.png", 27, 116, 4, 0, 0);//body
+	game.load.spritesheet("playerLegLeftSpriteSheet",
+		"assets/leftLegSpriteSheet.png", 19, 43, 2, 0, 0);//legs right
+	game.load.spritesheet("playerLegRightSpriteSheet",
+		"assets/rightLegSpriteSheet.png", 14, 45, 2, 0, 0);//legs right
 }
 
 function create()
@@ -64,21 +74,37 @@ function create()
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	game.stage.backgroundColor = "#000";
 
-	//for arrows keys
-    cursors = game.input.keyboard.createCursorKeys();
+	// keyboard control
+    input.cursors = game.input.keyboard.createCursorKeys();
+	input.esc = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+	input.esc.onDown.add(function()
+		{
+			switch (curr_state) {
+			case game_state.GAME:
+				curr_state = game_state.PAUSE;
+			break;
+			case game_state.PAUSE:
+				curr_state = game_state.GAME;
+			break;
+			}
+		});
 	// for game world size more than screen size
 	//game.world.setBounds( -1000, -1000, 1000, 1000);
 	game.world.setBounds( 0, 0, 800, 600);
 
-	text = game.add.text(game.world.centerX, game.world.centerY, "this game", styles[1]);
+	text = game.add.text(game.world.centerX,
+		game.world.centerY,
+		"this game",
+		styles[1]);
 	
 	// Player init
-	player.push( createPlayer(game, {x:100, y:10}, "#fac", ["playerHeadSpriteSheet",
-														  "playerBodySpriteSheet",
-														  "playerHandLeftSpriteSheet",
-														  "playerHandRightSpriteSheet",
-														  "playerLegLeftSpriteSheet",
-														  "playerLegRightSpriteSheet"], 300, 0.1));
+	player.push(createPlayer(game, {x:100, y:10}, "#fac",
+		["playerHeadSpriteSheet",
+			"playerBodySpriteSheet",
+			"playerHandLeftSpriteSheet",
+			"playerHandRightSpriteSheet",
+			"playerLegLeftSpriteSheet",
+			"playerLegRightSpriteSheet"], 300, 0.1));
 }
 
 function update()
@@ -89,26 +115,35 @@ function update()
 	switch (curr_state) {
 	case game_state.GAME:
 		//animation stay
-		if (cursors.up.isDown ||
-			cursors.down.isDown ||
-			cursors.left.isDown ||
-			cursors.right.isDown) {
+		if (input.cursors.up.isDown ||
+			input.cursors.down.isDown ||
+			input.cursors.left.isDown ||
+			input.cursors.right.isDown) {
+			// TODO set to run
 			player[0].setAnimation("stay");
 		}
 
-		if (cursors.up.isDown) {
+		if (input.cursors.up.isDown) {
 			// TODO
-		} else if (cursors.down.isDown) {
+		} else if (input.cursors.down.isDown) {
 			player[0].body.sprite.position.y += 6;
 		}
-		if (cursors.left.isDown) {
+
+		if (input.cursors.left.isDown) {
 			player[0].body.sprite.position.x -= 4;
 			player[0].dirrection = 1;
-		} else if (cursors.right.isDown) {
+		} else if (input.cursors.right.isDown) {
 			player[0].body.sprite.position.x += 4;
 			player[0].dirrection = -1;
 		}
 		break;
+	case game_state.PAUSE:
+		// TODO fade
+		text = game.add.text(game.world.centerX - 200,
+			game.world.centerY,
+			"Press ESC to contunue",
+			styles[0]);
+	break;
 	default: break;	
 	}
 
